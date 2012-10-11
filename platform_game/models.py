08d7105071f83326db1391644334a96016d7c22f
Matt Mendell <matthew.mendell@gmail.com>
2012-10-11 00:44:12 -0400

    Extract out platform retrieval

--- platform_game/models.py
+++ platform_game/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+import urllib2
+import xml.etree.ElementTree as ET
+#from django.db import models
 
-# Add models
+def party_url(party):
+    if party == 'Republican':
+        return 'https://raw.github.com/tauberer/party-platforms/master/republican_platform_2012.xml'
+    elif party == 'Democratic':
+        return 'https://raw.github.com/tauberer/party-platforms/master/democratic/democratic_platform_2012.xml'
+    elif party == 'Libertarian':
+        return 'https://raw.github.com/tauberer/party-platforms/master/libertarian_platform_2012.xml'
+
+# API
+def get_platform(party):
+
+    url = party_url(party)
+
+    # Fetch XML data
+    # FIXME: cache this
+    f = urllib2.urlopen(url)
+
+    doc =  ET.parse(f)
+
+    proposals = [x.text for x in list(doc.iter('proposal'))]
+
+    platform = {
+        'party': doc.find('party').text,
+        'year': doc.find('year').text,
+        'proposals': proposals,
+    }
+
+    return platform
+
--- platform_game/views.py
+++ platform_game/views.py
@@ -1,20 +1,12 @@
-import urllib2
-import xml.etree.ElementTree as ET
 from django.http import HttpResponse
 from django.template import Context, loader
+from platform_game.models import get_platform
 
 def index(request):
 
-    # Fetch XML data
-    # FIXME: cache this
-    f = urllib2.urlopen('https://raw.github.com/tauberer/party-platforms/master/republican_platform_2012.xml')
-
-    doc = ET.parse(f)
+    platform = get_platform('Republican')
 
     t = loader.get_template('platform_game/index.html')
-    c = Context({
-        'party': doc.find('party').text,
-        'proposal': doc.iter('proposal').next().text
-    })
+    c = Context(platform)
 
     return HttpResponse(t.render(c))
